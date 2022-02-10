5fb8d9bdb5ef6b7528cee72743684e72f577a191
audgks5551 <audgks5551@daum.net>
2022-02-10 15:46:22 +0900

    로그아웃 설정

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -53,6 +56,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
 
                 .permitAll() // 로그인 주소에 들어와야 인증이 가능하므로 모든 사용자 접근 허용
+                ;
+
+        http
+                .logout()
+                .logoutUrl("/logout") // 로그아웃 주소
+                .logoutSuccessUrl("/login") // 로그아웃 성공 시 리다이렉트할 주소
+
+                // 로그아웃할 때 실행하는 함수
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate(); // 세션 무효화
+                    }
+                })
+
+                // 로그아웃이 성공적으로 마쳤을 때 실행하는 함수
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me") // 쿠키 삭제
                 ;
 
     }
